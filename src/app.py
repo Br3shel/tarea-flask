1e3846080c47c9528daa56577689efa84b4cffd1
Claudino <brishel_acosta@hotmail.com>
2024-02-27 17:56:17 -0600

    Se agrego los métodos get and post

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 
 app=Flask(__name__)
 
@@ -10,6 +10,7 @@ def index():
         'nombre':'Claudino brishel Lopez Acosta'
     }
     return render_template('index.html', data=data)
+
 @app.route('/pagina2/')
 def pagina2():
     data={
@@ -18,13 +19,26 @@ def pagina2():
         'nombre':'Claudino brishel Lopez Acosta',
     }
     return render_template('pagina2.html', data=data)
+
 @app.route('/imagen/')
 def imagen():
     return render_template('imagen.html')
-@app.route('/login/')
-def login():
-    return render_template('login.html')
 
+@app.route('/')
+def inscrito():
+    return redirect(url_for('login'))
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        print(request.form['username'])
+        print(request.form['password'])
+        correo=(request.form['username'])
+        Password=(request.form['password'])
+        return render_template('inscrito.html', correo=correo , Password=Password)
+    else:    
+        return render_template('login.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
