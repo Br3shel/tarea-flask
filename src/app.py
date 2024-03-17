67cf0264adf3686c547ee4224672ee2677f02a57
Claudino <brishel_acosta@hotmail.com>
2024-03-16 20:45:35 -0600

    conexion a la base de datos

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template,request,redirect,url_for,flash,session
+from flask_mysqldb import MySQL
+import config
 
 app=Flask(__name__)
+
+app.config["MYSQL_USER"]= config.MYSQL_USER
+app.config["MYSQL_DB"]= config.MYSQL_DB
+app.config["MYSQL_PASSWORD"]= config.MYSQL_PASSWORD
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+db = MySQL(app)
 
 @app.route('/')
 def index():
@@ -24,21 +32,35 @@ def pagina2():
 def imagen():
     return render_template('imagen.html')
 
-@app.route('/')
-def inscrito():
-    return redirect(url_for('login'))
-
 @app.route('/login/', methods=['GET','POST'])
 def login():
-    if request.method=='POST':
-        print(request.form['username'])
-        print(request.form['password'])
-        correo=(request.form['username'])
-        Password=(request.form['password'])
-        return render_template('inscrito.html', correo=correo , Password=Password)
-    else:    
-        return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if email and password:
+            cur = db.connection.cursor()
+            cur.execute("SELECT * FROM user WHERE email = %s AND password = %s", (email, password))
+            user = cur.fetchone()
+            cur.close()
+            if user is not None:
+                session['email'] = email
+                session['name'] = user[1]
+                session['surnames'] = user[2]
+                return redirect(url_for('inscrito'))
+            
+        return render_template('login.html', message="Email o contraseña incorrectos")
     
+    return render_template('login.html')
+   
+@app.route('/inscrito')
+def inscrito():
+    return render_template('inscrito.html')   
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear() 
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ src/config.py
@@ -0,0 +1,7 @@
+
+DEBUG = True
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+MYSQL_DB = 'login' 
+HEX_SEC_KEY= 'd5fb8c4fa8bd46638dadc4e751e0d68d'
